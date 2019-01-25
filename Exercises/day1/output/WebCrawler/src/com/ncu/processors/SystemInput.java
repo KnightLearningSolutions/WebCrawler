cf7a4a2feed39957fa0d5bf4ae7f932d209f3665
adityapalmehula <javajiadityapal@gmail.com>
2019-01-25 09:43:43 +0530

    add labs for content

--- Exercises/day1/output/WebCrawler/src/com/ncu/processors/SystemInput.java
+++ Exercises/day1/output/WebCrawler/src/com/ncu/processors/SystemInput.java
@@ -51,87 +51,86 @@ public class SystemInput
   	Scanner scan = new Scanner(System.in);
   	String urlName = scan.nextLine();
     
-     /* Calling searchValue only for Testing Purpose this line will delete further session  */
-    systemInputObj.searchValue(urlName);
+   /* Calling searchValue only for Testing Purpose this line will delete further session  */
+   systemInputObj.searchValue(urlName);
 
-  }
+ }
 
   // This method will take Search Word from user , which user want to search on url
-  public void searchValue(String urlName)
-  {
+ public void searchValue(String urlName)
+ {
 		// Creating object of current class object to use in this method only.
-  	SystemInput systemInputObj=new SystemInput();
+   SystemInput systemInputObj=new SystemInput();
 
     // Taking Url as a input from user.
-  	System.out.println("\n");
-  	logger.info("Enter Your String Which You Want to Search :- ");
-  	Scanner scan = new Scanner(System.in);
-  	String searchValue= scan.nextLine();
+   System.out.println("\n");
+   logger.info("Enter Your String Which You Want to Search :- ");
+   Scanner scan = new Scanner(System.in);
+   String searchValue= scan.nextLine();
 
-    /* Calling searchValue only for Testing Purpose this line will delete further session  */
-    systemInputObj.numberOfPages(urlName,searchValue);
+   /* Calling searchValue only for Testing Purpose this line will delete further session  */
+   systemInputObj.numberOfPages(urlName,searchValue);
 
-  } 
+ } 
 
   // This method will take how number of pages do you want to search. 
-  public void numberOfPages(String urlName,String searchValue)
-  {
+ public void numberOfPages(String urlName,String searchValue)
+ {
 		// Creating object of current class object to use in this method only.
-  	SystemInput systemInputObj=new SystemInput();
+   SystemInput systemInputObj=new SystemInput();
 
 		// Using Try Catch if to catch exception if user give string value as number of page to search
-  	try{
+   try{
 		// Taking number of pages as a input from user.
-  		System.out.println("\n");
-  		logger.info("How Many Pages Do You Want To Search :- ");
-  		Scanner scan = new Scanner(System.in);
-  		int numberOfPage= scan.nextInt();
+    System.out.println("\n");
+    logger.info("How Many Pages Do You Want To Search :- ");
+    Scanner scan = new Scanner(System.in);
+    int numberOfPage= scan.nextInt();
 
     /* Calling searchValue only for Testing Purpose this line will delete further session  */
     systemInputObj.writeOutputInFile();
 
-  	}catch(InputMismatchException e){
-  		logger.error("\n"+e+message.getProperty("validIntegerInput")+"\n");
-  		systemInputObj.numberOfPages(urlName,searchValue);
-  	}
+  }catch(InputMismatchException e){
+    logger.error("\n"+e+message.getProperty("validIntegerInput")+"\n");
+    systemInputObj.numberOfPages(urlName,searchValue);
   }
+}
 
   // writeOutputInFile Method to write output into json file .// URLCrawler urlCrawlerObj
-  public void writeOutputInFile()
-  { 
+public void writeOutputInFile()
+{ 
 		// Creating object of current class object to use in this method only.
-  	SystemInput systemInputObj=new SystemInput();
+ SystemInput systemInputObj=new SystemInput();
 
     // Taking Url as a input from user.
-  	System.out.println("\n");
-  	logger.info("Please Enter Your Json File Name :- ");
-  	Scanner scan = new Scanner(System.in);
-  	String fileName= scan.nextLine();
-    
-    /* Calling searchValue only for Testing Purpose this line will delete further session  */
-    systemInputObj.exits();
+ System.out.println("\n");
+ logger.info("Please Enter Your Json File Name :- ");
+ Scanner scan = new Scanner(System.in);
+ String fileName= scan.nextLine();
+ 
+ /* Calling searchValue only for Testing Purpose this line will delete further session  */
+ systemInputObj.exits();
 
-	}
+}
 
 	// Implementation of Exit from System
-	public void exits(){
+public void exits(){
 
 		// Creating object of current class object to use in this method only.
-		SystemInput systemInputObj=new SystemInput(); 
+  SystemInput systemInputObj=new SystemInput(); 
 
   	// Ask user to exit from system or Continue process.
-		System.out.println("\n");
-		logger.info(message.getProperty("exitMessage"));
-		Scanner exitObject = new Scanner(System.in);
-		String userPermisson= exitObject.nextLine();
-		String setPermisson = constants.getProperty("endProcess");
-		if(setPermisson.equalsIgnoreCase(userPermisson)){
-			logger.info("Thanks for Using Our System .......!");
-			System.exit(0);
-		}else{
+  System.out.println("\n");
+  logger.info(message.getProperty("exitMessage"));
+  Scanner exitObject = new Scanner(System.in);
+  String userPermisson= exitObject.nextLine();
+  String setPermisson = constants.getProperty("endProcess");
+  if(setPermisson.equalsIgnoreCase(userPermisson)){
+   logger.info("Thanks for Using Our System .......!");
+   System.exit(0);
+ }else{
 		// Continue our process Again.
-			systemInputObj.inputUrl();
-		} 
-	}
-
+   systemInputObj.inputUrl();
+ } 
+}
 } 
